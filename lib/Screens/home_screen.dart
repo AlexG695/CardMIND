41cbfe06f375ed80c458a2e40b48f9a1c8ad6a2b
AlexG695 <alexg695845@gmail.com>
2022-08-08 03:24:57 -0600

    Condicion data 2

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -75,7 +75,7 @@ class HomeScreenState extends State<HomeScreen> {
                 stream: coleccionLDR.doc("sh3C4XK69wRtO24Pg59Q").snapshots(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   if (snapshot.connectionState == ConnectionState.active) {
-                    if (snapshot.data == null) {
+                    if (snapshot.data.data()["Estado"] == null) {
                       return const Text(
                           'No se detecta nada cerca del automovil');
                     }
@@ -90,7 +90,7 @@ class HomeScreenState extends State<HomeScreen> {
                 stream: coleccionLDR.doc("sh3C4XK69wRtO24Pg59Q").snapshots(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   if (snapshot.connectionState == ConnectionState.active) {
-                    if (snapshot.data == null) {
+                    if (snapshot.data.data()["Mensaje"] == null) {
                       return const Text("La puerta está cerrada");
                     }
                     return Text(
@@ -104,6 +104,9 @@ class HomeScreenState extends State<HomeScreen> {
                 stream: coleccionULTRA.doc("676lC9BKFOLSivyA8wB3").snapshots(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   if (snapshot.connectionState == ConnectionState.active) {
+                    if (snapshot.data.data1()["Estado"] == null) {
+                      return const Text("Desactivado");
+                    }
                     return Text(
                         snapshot.data.data1()["Estado"] ?? "Desactivado");
                   } else {
@@ -120,6 +123,9 @@ class HomeScreenState extends State<HomeScreen> {
                 stream: coleccionULTRA.doc("676lC9BKFOLSivyA8wB3").snapshots(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
                   if (snapshot.connectionState == ConnectionState.active) {
+                    if (snapshot.data.data()["Mensaje"] == null) {
+                      return const Text("No hay objetos cercanos");
+                    }
                     return Text(snapshot.data.data()["Mensaje"] ??
                         "No hay objetos cercanos");
                   } else {
